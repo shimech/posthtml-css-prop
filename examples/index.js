774df43bf62262880066640244403fb20178f4b0
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-03-03 13:15:52 +0900

    add class

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,9 @@
 const posthtml = require("posthtml");
 const html = `
-    <div>Hello World!</div>
+    <h1 css-prop="font-size: 24px">Title</h1>
+    <div class="foo" css-prop="text-align: center">
+        <span css-prop="color: red">Hello World!</span>
+    </div>
 `;
 
 posthtml()
